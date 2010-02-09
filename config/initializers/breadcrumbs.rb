3192fe42c92726f5d145975797ed2611c4902bba
Diman <devandart@ya.ru>
2010-02-09 04:19:38 +0300

    add nvigation

--- config/initializers/breadcrumbs.rb
+++ config/initializers/breadcrumbs.rb
@@ -1,14 +1,29 @@
 Breadcrumb.configure do
   dont_link_last_crumb
   # Specify name, link title and the URL to link to
-  crumb :profile, "Your Profile", :account_url
   crumb :root, "Главная", :root_url
-  crumb :debate, "Новый спор", :new_debate_url
+  crumb :new_debate, "Новый спор", :new_debate_url
+  crumb :show_debate, "Спор/мнение", :new_debate_url
+  crumb :debates_comments, "Спор/мнение", :debate_url, '@debate'
+  crumb :new_comment, "Новый комментарий", :new_comment_url
+  crumb :mydebates, "Мои споры/мнения", :mydebates_debates_url
+  crumb :edit_user, "Мои настройки", :edit_users, '@users'
+  crumb :new_user, "Регистрация", :new_user
+  crumb :login, "Авторизация", :login_url
+
 
   # Specify controller, action, and an array of the crumbs you specified above
-  trail :debates, :new, [:root, :debate]
-  trail :accounts, :show, [:root, :profile]
+  trail :debates, :new, [:root, :new_debate]
+  trail :debates, :show, [:root, :show_debate]
+  trail :debates, :mydebates, [:root, :mydebates]
   trail :home, :index, [:root]
+  trail :comments, :new, [:root, :debates_comments, :new_comment]
+  trail :comments, :create, [:root, :debates_comments, :new_comment]
+  trail :users, :edit, [:root, :edit_user]
+  trail :users, :new, [:root, :new_user]
+  trail :users, :create, [:root, :new_user]
+  trail :user_sessions, :new, [:root, :login]
+
 
   # Specify the delimiter for the crumbs
   delimit_with " > "
