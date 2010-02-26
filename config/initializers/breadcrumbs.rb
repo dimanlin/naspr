7d3e143d1646a16fd8892793d466749d20c03fad
Diman <devandart@ya.ru>
2010-02-27 01:47:55 +0300

    Коегде добавил навигацию

--- config/initializers/breadcrumbs.rb
+++ config/initializers/breadcrumbs.rb
@@ -5,6 +5,7 @@ Breadcrumb.configure do
   crumb :new_debate, "Новый спор", :new_debate_url
   crumb :show_debate, "Спор/мнение", :new_debate_url
   crumb :debates_comments, "Спор/мнение", :debate_url, '@debate'
+  crumb :index_debates, "Все споры", :debate_url
   crumb :new_comment, "Новый комментарий", :new_comment_url
   crumb :mydebates, "Мои споры/мнения", :mydebates_debates_url
   crumb :edit_user, "Мои настройки", :edit_users, '@users'
@@ -12,12 +13,15 @@ Breadcrumb.configure do
   crumb :login, "Авторизация", :login_url
   crumb :debate_search, "Поиск", :debate_search_url
   crumb :contact, "Контакты", "asdas"
+  crumb :recover_password, "Продседура востановления пароля", :recover_password_url
+  crumb :newpassword, "Продседура востановления пароля", :recover_password_url
 
 
   # Specify controller, action, and an array of the crumbs you specified above
   trail :debates, :new, [:root, :new_debate]
   trail :debates, :show, [:root, :show_debate]
   trail :debates, :mydebates, [:root, :mydebates]
+  trail :debates, :index, [:root, :index_debates]
   trail :home, :index, [:root]
   trail :comments, :new, [:root, :debates_comments, :new_comment]
   trail :comments, :create, [:root, :debates_comments, :new_comment]
@@ -28,6 +32,8 @@ Breadcrumb.configure do
   trail :debates, :search, [:root, :debate_search]
   trail :contents, :show, [:root, :contact]
   trail :contacts, :index, [:root, :contact]
+  trail :user_sessions, :recover_password, [:root, :recover_password]
+  trail :users, :newpassword, [:root, :newpassword]
 
 
   # Specify the delimiter for the crumbs
